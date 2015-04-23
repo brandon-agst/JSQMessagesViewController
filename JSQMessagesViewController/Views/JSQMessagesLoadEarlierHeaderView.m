ac3afadda1df16f0a8bb2c4e5b08f1e52f08627c
brandon-agst <brandonfriend@Brandons-MacBook-Pro.local>
2015-04-23 11:45:05 -0700

    JSQMessagesLoadEarlierHeaderView: Adjust style of headerView

--- JSQMessagesViewController/Views/JSQMessagesLoadEarlierHeaderView.m
+++ JSQMessagesViewController/Views/JSQMessagesLoadEarlierHeaderView.m
@@ -60,7 +60,7 @@ const CGFloat kJSQMessagesLoadEarlierHeaderViewHeight = 32.0f;
     self.backgroundColor = [UIColor clearColor];
 
     [self.loadButton setTitle:[NSBundle jsq_localizedStringForKey:@"load_earlier_messages"] forState:UIControlStateNormal];
-    self.loadButton.titleLabel.font = [UIFont preferredFontForTextStyle:UIFontTextStyleBody];
+//    self.loadButton.titleLabel.font = [UIFont preferredFontForTextStyle:UIFontTextStyleBody];
 }
 
 - (void)dealloc
